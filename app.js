02a7681744edc67d19434772d4d1e4a379f1a3a5
LFF5644 (Code-Server) <meerschweinchen5644@gmail.com>
2023-03-17 22:05:17 +0100

    add edit mode

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const model={
 		tasks: [],
 		taskText: "",
 		taskTitle: "",
+		view: "tasks",
 	}),
 	setTasks:(state,tasks)=>({
 		...state,
@@ -74,6 +75,10 @@ const model={
 		...state,
 		taskText,
 	}),
+	setView:(state,view)=>({
+		...state,
+		view,
+	}),
 };
 
 function getToken(){
@@ -84,6 +89,11 @@ function getToken(){
 function ViewTasks({socket,state,actions}){return[
 	state.tasks.length===0&&
 	node_dom("h1[innerText=Keine TODOs]"),
+	node_dom("p[innerText=Aufgabe jz ]",null,[
+		node_dom("a[innerText=erstellen]",{
+			onclick:()=> actions.setView("addTask"),
+		}),
+	]),
 
 	state.tasks.length>0&&
 	node_dom("table",{
@@ -113,14 +123,15 @@ function Task({I,socket,state,actions}){return[
 				finished: I.finished,
 			},
 		}),
+		node_dom("td",null,[
+			node_dom("button[innerText=E]",{
+				onclick:()=> actions.setView(I.id),
+			}),
+		]),
 	]),
 ]}
 function ViewAddTask({socket,state,actions}){return[
-	node_dom("div",{
-		F:{
-			addTask: true,
-		},
-	},[
+	node_dom("div[className=box]",null,[
 		node_dom("p",null,[
 			node_dom("label[innerText=Titel: ]",null,[
 				node_dom("input",{
@@ -137,16 +148,64 @@ function ViewAddTask({socket,state,actions}){return[
 				}),
 			]),
 		]),
-		node_dom("button[innerText=ADD TODO]",{
-			onclick:()=> actions.addTask({
-				id: Date.now(),
-				title: state.taskTitle,
-				text: state.taskText,
+		node_dom("p",null,[
+			node_dom("button[innerText=ADD TODO]",{
+				onclick:()=>{
+					actions.addTask({
+						id: Date.now(),
+						title: state.taskTitle,
+						text: state.taskText,
+					});
+					actions.setView("tasks");
+					actions.setTaskText("");
+					actions.setTaskTitle("");
+				},
 			}),
-		}),
+			node_dom("button[innerText=Zurück]",{
+				onclick:()=> actions.setView("tasks"),
+			}),
+			node_dom("button[innerText=Säubern]",{
+				onclick:()=>{
+					actions.setTaskText("");
+					actions.setTaskTitle("");
+				},
+			}),
+		]),
+		
 	]),
 ]}
-
+function ViewEdit({state,actions,task}){return[
+	node_dom("h1[className=withButton]",null,[
+		node_dom("button[innerText=Back]",{
+			onclick:()=> actions.setView("tasks"),
+		}),
+		node_dom("span[innerText=Bearbeiten]"),
+	]),
+	node_dom("div[className=box]",null,[
+		node_dom("p",null,[
+			node_dom("label[innerText=Titel: ]",null,[
+				node_dom("input",{
+					oninput: event=> actions.editTask({
+						id: task.id,
+						title: event.target.value,
+					}),
+					value: task.title,
+				}),
+			]),
+		]),
+		node_dom("p",null,[
+			node_dom("label[innerText=Text: ]",null,[
+				node_dom("input",{
+					oninput: event=> actions.editTask({
+						id: task.id,
+						text: event.target.value,
+					}),
+					value: task.text,
+				}),
+			]),
+		]),
+	]),
+]}
 
 init(()=>{
 	const [state,actions]=hook_model(model);
@@ -174,7 +233,16 @@ init(()=>{
 		return socket;
 	});
 	return[null,[
-		node(ViewAddTask,{socket,state,actions}),
+		state.view==="tasks"&&
 		node(ViewTasks,{socket,state,actions}),
+
+		state.view=="addTask"&&
+		node(ViewAddTask,{socket,state,actions}),
+		
+		typeof(state.view)==="number"&&
+		node(ViewEdit,{
+			state, actions,
+			task: state.tasks.find(item=>item.id===state.view),
+		}),
 	]];
 });
